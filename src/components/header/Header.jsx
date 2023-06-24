eb8f43a4a45ee60140df78c7f57b3e4a287dcd82
HarryShots <bobgalaharrison8@gmail.com>
2023-06-24 15:00:03 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello, I'm</h5>
         <h1>Bobgala Harrison Dima</h1>
-        <h5 className="text-light">Photographer/Videographer</h5>
+        <h5 className="text-light">Photographer/Videographer, Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
 
